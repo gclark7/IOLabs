b3c8c2832a16eca23bdca29a5820dee5c6004125
gcDataTechnology <gcDataTechnology@gcDataTechnolog>
2013-11-06 16:22:40 -0600

    I/O laboratory 1

--- src/lab1/BeautifulRose.java
+++ src/lab1/BeautifulRose.java
@@ -1,6 +1,6 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Playing with generics...This does not do what I had hoped
+ * Not really a composition technique.
  */
 package lab1;
 
@@ -11,6 +11,8 @@ package lab1;
 public class BeautifulRose <T extends Rose & Flower> {
     //jdk 7 api says "A type variable with multiple bounds is a subtype of all the types listed in the bound. 
     //If one of the bounds is a class, it must be specified first."
+    
+    //Thought this may be a way of extending a super class or interface
     
     
     
